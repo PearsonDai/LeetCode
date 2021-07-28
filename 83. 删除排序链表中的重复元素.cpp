a13e07e3c5899f4d2fb5c18b6ec8bda645d956e2
PearsonDai <87842114+PearsonDai@users.noreply.github.com>
2021-07-28 10:19:11 +0800

    Add files via upload

---
+++ 83. 删除排序链表中的重复元素.cpp
@@ -0,0 +1,40 @@
+83. 删除排序链表中的重复元素
+存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+
+返回同样按升序排列的结果链表。
+
+ 
+
+示例 1：
+输入：head = [1,1,2]
+输出：[1,2]
+
+示例 2：
+输入：head = [1,1,2,3,3]
+输出：[1,2,3]
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *p=head;
+        while(p){
+            if(p->next&&p->next->val==p->val){
+                p->next=p->next->next;
+            }
+            else{
+                p=p->next;
+            }
+        }
+        return head;
+    }
+};
