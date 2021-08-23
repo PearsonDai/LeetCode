2c87eac57850bb7c76bdf6617fbb072e81cbc68c
PearsonDai <87842114+PearsonDai@users.noreply.github.com>
2021-08-23 10:21:00 +0800

    Add files via upload

---
+++ 543. 二叉树的直径.cpp
@@ -0,0 +1,41 @@
+543. 二叉树的直径
+给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
+
+示例 :
+给定二叉树
+
+          1
+         / \
+        2   3
+       / \     
+      4   5    
+返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+
+注意：两结点之间的路径长度是以它们之间边的数目表示。
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDiam=0;
+    int diameterOfBinaryTree(TreeNode* root) {
+        dfs(root);
+        return maxDiam;
+    }
+    int dfs(TreeNode* root){
+        if(!root) return 0;
+        int left=dfs(root->left);
+        int right=dfs(root->right);
+        maxDiam=max(maxDiam,left+right);
+        return max(left,right)+1;
+    }
+};
