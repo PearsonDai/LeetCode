26a7dea2f472ca03bf6dd6e3e45120624abd7395
PearsonDai <87842114+PearsonDai@users.noreply.github.com>
2021-08-31 17:20:51 +0800

    Add files via upload

---
+++ 剑指 Offer 53 - II. 0～n-1中缺失的数字.cpp
@@ -0,0 +1,42 @@
+剑指 Offer 53 - II. 0～n-1中缺失的数字
+一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+
+示例 1:
+
+输入: [0,1,3]
+输出: 2
+示例 2:
+
+输入: [0,1,2,3,4,5,6,7,9]
+输出: 8
+ 
+
+限制：
+
+1 <= 数组长度 <= 10000
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int left=0,right=nums.size()-1;
+        while(left<right){
+            int mid=(left+right)/2;
+            if(nums[mid]==mid) left=mid+1;
+            else right=mid;
+        }
+        return (left==nums.size()-1&&nums[left]==left)?left+1:left;
+    }
+};
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int l=0,r=nums.size()-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(nums[mid]==mid) l=mid+1;
+            else r=mid-1;
+        }
+        return l;
+    }
+};
---
+++ 剑指 Offer 54. 二叉搜索树的第k大节点.cpp
@@ -0,0 +1,56 @@
+剑指 Offer 54. 二叉搜索树的第k大节点
+给定一棵二叉搜索树，请找出其中第k大的节点。
+
+示例 1:
+
+输入: root = [3,1,4,null,2], k = 1
+   3
+  / \
+ 1   4
+  \
+   2
+输出: 4
+示例 2:
+
+输入: root = [5,3,6,2,4,null,null,1], k = 3
+       5
+      / \
+     3   6
+    / \
+   2   4
+  /
+ 1
+输出: 4
+ 
+
+限制：
+
+1 ≤ k ≤ 二叉搜索树元素个数
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int kthLargest(TreeNode* root, int k) {
+        stack<TreeNode*> stk;
+        while(root||!stk.empty()){
+            while(root){
+                stk.push(root);
+                root=root->right;
+            }
+            root=stk.top();
+            stk.pop();
+            k--;
+            if(k==0) return root->val;
+            root=root->left;
+        }
+        return -1;
+    }
+};
