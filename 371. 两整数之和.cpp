7e6cce500b4ce0b88b9aa403fb191623d922f237
PearsonDai <87842114+PearsonDai@users.noreply.github.com>
2021-09-26 16:13:36 +0800

    Add files via upload

---
+++ 371. 两整数之和.cpp
@@ -0,0 +1,28 @@
+371. 两整数之和
+给你两个整数 a 和 b ，不使用 运算符 + 和 - ​​​​​​​，计算并返回两整数之和。
+
+示例 1：
+
+输入：a = 1, b = 2
+输出：3
+示例 2：
+
+输入：a = 2, b = 3
+输出：5
+ 
+
+提示：
+
+-1000 <= a, b <= 1000
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while(b){
+            int temp = (a ^ b);
+            b = (unsigned)(a & b) << 1;
+            a = temp;
+        }
+        return a;
+    }
+};
